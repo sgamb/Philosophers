2810ae36ea544a566e15f0d3e3ab5aa49336bfe0
Sergey Gambaryan <sgamb2000@gmail.com>
2023-11-06 18:19:31 +0400

    They are sleeping and waitng to join <3

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <pthread.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
 typedef struct	s_philo
 {
@@ -17,17 +20,45 @@ int main(void)
 	int			i;
 	pthread_t	philo_ids[N];
 	t_philo		philos[N];
+	struct timeval tv;
 
-	i = N;
-	while (i--)
-		pthread_create(&philo_ids[i], NULL, &philo_routine, &philos[i]);
+	gettimeofday(&tv, NULL);
+	srandom(tv.tv_sec);
+	i = 0;
 	while (i < N)
+	{
+		philos[i].num = i;
+		i++;
+	}
+	i = 0;
+	while (i < N)
+	{
+		pthread_create(&philo_ids[i], NULL, &philo_routine, &philos[i]);
+		i++;
+	} 
+	i = 0;
+	// usleep(1000);
+	while (i < N)
+	{
 		pthread_join(philo_ids[i++], NULL);
+		gettimeofday(&tv, NULL);
+		printf("joined %dth thread\ttime is %d\n", i-1, tv.tv_usec / 1000);
+	}
 	return (0);
 }
 
 void	*philo_routine(void *philo_data)
 {
-	printf("I'm started\n");
+	t_philo	*data;
+	struct timeval tv;
+	int	sleep_time;
+
+	data = (t_philo *)philo_data;
+	gettimeofday(&tv, NULL);
+	sleep_time = (0 + data->num) % 5 * 2000;
+	printf("started %d\tcurrent time is %d\tgoing to sleep for %d milliseconds\n", data->num, tv.tv_usec / 1000, sleep_time / 1000);
+	usleep(sleep_time);
+	gettimeofday(&tv, NULL);
+	printf("ended %d\tcurrent time is %d\n", data->num, tv.tv_usec / 1000);
 	return (NULL);
 }
