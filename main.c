db07c2047916ae5442426a0ffe3c15ec7fd0eb22
Sergey Gambaryan <sgamb2000@gmail.com>
2023-11-26 18:52:29 +0400

    Working simulation

--- main.c
+++ main.c
@@ -16,8 +16,10 @@ typedef struct	s_philo
 void			*philo_routine(void *philo_data);
 pthread_mutex_t	print;
 
-#define N 2
-#define TIME_TO_DIE 1000
+#define N 5
+#define TIME_TO_DIE 6000
+#define TIME_TO_EAT 800000
+#define TIME_TO_SLEEP 800000
 #define MEAL_NUM 5
 #define TRUE 1
 #define FALSE 0
@@ -112,6 +114,7 @@ void	ft_wait_philosophers(t_philo *philos)
 		pthread_join(philos[i++].id, NULL);
 }
 
+// void	take_fork(philo, fork);
 void	*philo_routine(void *philo_data)
 {
 	t_philo			*data;
@@ -122,30 +125,34 @@ void	*philo_routine(void *philo_data)
 	sleep_time = (1 + (data->num % N)) * 100000;
 	while (data->until)
 	{
-		// take first fork
-		if (data->num == 0)
+		if (data->num % 2 == 0)
 		{
-			pthread_mutex_lock(&forks[data->num]);
+			pthread_mutex_lock(&forks[data->num]); // take left
 			gettimeofday(&tv, NULL);
 			printf("%ld.%04d %d has taken left fork %d\n", tv.tv_sec % 100, tv.tv_usec / 10000, data->num, data->num);
-			pthread_mutex_lock(&forks[(data->num + 1) % N]);
+			pthread_mutex_lock(&forks[(data->num + 1) % N]); // take right
 			gettimeofday(&tv, NULL);
 			printf("%ld.%04d %d has taken right fork %d\n", tv.tv_sec % 100, tv.tv_usec / 10000, data->num, (data->num + 1));
 		}
 		else
 		{
-			pthread_mutex_lock(&forks[(data->num + 1) % N]);
+			pthread_mutex_lock(&forks[(data->num + 1) % N]); // take right
 			gettimeofday(&tv, NULL);
 			printf("%ld.%04d %d has taken right fork %d\n", tv.tv_sec % 100, tv.tv_usec % 10000, data->num, (data->num + 1) % N);
-			pthread_mutex_lock(&forks[data->num]);
+			pthread_mutex_lock(&forks[data->num]); // take left
 			gettimeofday(&tv, NULL);
 			printf("%ld.%04d %d has taken left fork %d\n", tv.tv_sec % 100, tv.tv_usec % 10000, data->num, data->num);
 		}
 		gettimeofday(&tv, NULL);
 		printf("%ld.%04d %d is eating\n", tv.tv_sec % 100, tv.tv_usec % 10000, data->num);
-		usleep(sleep_time);
+		usleep(TIME_TO_EAT);
 		pthread_mutex_unlock(&forks[data->num]);
 		pthread_mutex_unlock(&forks[(data->num + 1) % N]);
+		gettimeofday(&tv, NULL);
+		printf("%ld.%04d %d is sleeping\n", tv.tv_sec % 100, tv.tv_usec % 10000, data->num);
+		usleep(TIME_TO_SLEEP);
+		gettimeofday(&tv, NULL);
+		printf("%ld.%04d %d is thinking\n", tv.tv_sec % 100, tv.tv_usec % 10000, data->num);
 		data->meal_num++;
 	}
 	return (NULL);
